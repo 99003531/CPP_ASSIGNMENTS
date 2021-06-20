9ac0193cc16b4cec3ce2f89498939120649f21e6
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 13:35:06 +0530

    Update main.cpp

--- Complex/main.cpp
+++ Complex/main.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include "Complex.h"
+using namespace std;
 
+int main()
+{
+
+        complex c1, c2;
+        int n;
+        do
+        {
+                cout<<"\n 1. Input data for complex no. ";
+                cout<<"\n 2. Addition of complex no. ";
+                cout<<"\n 3. Subtraction of complex no. ";
+                cout<<"\n 4. Quit";
+                cout<<"\n Enter your choice :: ";
+                cin>>n;
+                switch(n)
+
+                {
+                         case 1:
+                         cout<<endl<<"\n Enter the data for First Complex No......";
+                         c1.getdata();
+                         cout<<endl<<"\n Enter the data for seconds Complex No.....";
+                         c2.getdata();
+
+                         break;
+
+                         case 2:
+                         c1+c2;
+
+                         break;
+
+                         case 3:
+                         c1-c2;
+
+                         break;
+
+                         case 4:
+                         exit(1);
+                         break;
+                        }
+                } while (n!=4);
+     return 0;
+}
