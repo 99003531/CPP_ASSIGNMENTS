f759e07c486f8df3d60c680486d9b31ca5f5f1da
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 13:00:49 +0530

    Update main.c

--- Image/main.c
+++ Image/main.c
@@ -1,1 +1,13 @@
+#include <iostream>
+#include "Image.h"
 
+using namespace std;
+int main(){
+Image r1(10,11,12,2);
+Image r2(2,3);
+cout<<"The image is moved by  "<< r2.move();<endl;
+Image r3(3);
+cout<<"The image is scaled by"<< r2.scale()<<endl;
+Image r3(2,5);
+cout<<"The volume of box is "<<r3.resize()<<endl;
+}
