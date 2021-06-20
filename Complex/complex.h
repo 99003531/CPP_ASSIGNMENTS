b43cac35a4acd1e48fcbafeacca388b58689de4e
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 13:03:23 +0530

    Update complex.h

--- Complex/complex.h
+++ Complex/complex.h
@@ -1,1 +1,17 @@
+#ifndef _COMPLEX_H_
+#define _COMPLEX_H_
 
+#include <string>
+
+class complex{
+   
+        public:
+            void getdata();
+            void operator + (complex);
+           void operator - (complex);
+
+        private:
+                  int real;
+                  float image;
+    
+};
