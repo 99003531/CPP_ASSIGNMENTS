d615be64e220b14512e8ab43b8dc39b7088eb8be
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 12:53:29 +0530

    Update Account.cpp

--- Account/Account.cpp
+++ Account/Account.cpp
@@ -1,1 +1,70 @@
 
+Conversation opened. 1 unread message.
+
+Skip to content
+Using Gmail with screen readers
+Meet
+New meeting
+Join a meeting
+Hangouts
+
+1 of 15,350
+Fwd: accountBase inheritance
+Inbox
+
+harshitha .r
+Attachments
+12:52 PM (0 minutes ago)
+to me
+
+
+---------- Forwarded message ---------
+From: RAJATH P D <rajathpd98@gmail.com>
+Date: Sun, Jun 20, 2021, 11:42 AM
+Subject: accountBase inheritance
+To: <harshirharshitha@gmail.com>
+
+
+
+
+3 Attachments
+#include "AccountBase.h"
+
+AccountBase::AccountBase()
+{
+    accNumber = "#0000";
+    accName = "Someone";
+    balance = 0;
+}
+AccountBase::AccountBase(std::string AccNo="1111",std::string AccName="alex" ,double bal=0)
+{
+    accNumber = AccNo;
+    accName = AccName;
+    balance = bal;
+}
+AccountBase:: AccountBase(const AccountBase&a1)
+{
+    accNumber = a1.accNumber;
+    accName = a1.accName;
+    balance = a1.balance;
+}
+ AccountBase::int debit(double)
+{
+    double amount;
+    cin>>this->amount;
+    balance = balance - amount;
+    return balance;
+}
+ AccountBase::int credit(double)
+{
+    double amount;
+    cin>>this->amount;
+    balance+=amount;
+    return balance;
+}
+AccountBase::void display()
+{
+    cout<<"Balance: "<<balance<<endl;
+}
+AccountBase.cpp
+Displaying AccountBase.cpp.
