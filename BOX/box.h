cdb7fcd89ca59322ceb992e489d4f10e9aebd3ea
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 12:48:28 +0530

    Update box.h

--- BOX/box.h
+++ BOX/box.h
@@ -1,1 +1,28 @@
 
+#ifndef _BOX_H_
+#define _BOX_H_
+
+#include <string>
+
+class Box{
+   
+        public:
+             Box();
+             Box(int, int, int); 
+             Box(Box &r);
+             int length() {return length};
+             int breadth() {return breadth};
+             int height() {return height};
+             void volume();
+             void get_length(int l);
+             void get_breadth(int b);
+             void get_height(int h);
+             void display();
+
+        private:
+                 int length;
+	          int breadth;
+	          int height;
+            int volume;
+    
+};
