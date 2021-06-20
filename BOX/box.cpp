1852401865f4569631515492faa4a0dc0375f5c5
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 12:46:28 +0530

    Update box.cpp

--- BOX/box.cpp
+++ BOX/box.cpp
@@ -1,1 +1,41 @@
 
+#include <string>
+#include <iostream>
+#include "Box.h"
+
+
+    Box::Box(){
+       length=1;
+       breadth=1;
+       height=1;
+}
+   
+
+     Box::Box(int l,int b,int h)){
+       length=l;
+       breadth=b;
+       height=h;
+      }
+     
+     void Box::get_length(int l)
+    {
+         length=l;
+    
+    }
+    void Box::get_breadth(int b)
+    {
+         breadth=b;
+    
+    }
+    void Box::get_height(int h)
+    {
+         height=h;
+     }
+    void Box::volume()
+    {
+        volume= length*breadth*height;
+    }
+     void Box::display()
+{
+cout<<"volume is "<<volume<<endl;
+}
