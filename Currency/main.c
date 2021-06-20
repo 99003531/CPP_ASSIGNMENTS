650bd28a5c5715241ab20a2c3f113b3b776b458f
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 13:26:58 +0530

    Update main.c

--- Currency/main.c
+++ Currency/main.c
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "Currency.h"
 
+using namespace std;
+
+int main(){
+    
+    cout << "Attempting to create a money object with $12.34\n";
+    Money money(12,34);
+    cout << money << endl;
+    
+    cout << "Input your own value as a float: ";
+    Money money2;
+    cin >> money2;
+    cout << money2 << endl;
+    
+    cout << "The total of the two values is " << money+money2 << endl;
+    cout << "The difference of the two values is " << money-money2 << endl;
+    
+    
+    return 0;
+}
