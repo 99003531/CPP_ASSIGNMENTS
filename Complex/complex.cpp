77b5a2f57e29ea7aee0ff83b721ccc62f7d916d4
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 13:04:31 +0530

    Update complex.cpp

--- Complex/complex.cpp
+++ Complex/complex.cpp
@@ -1,1 +1,42 @@
+void complex::getdata()
+{
+                cout<<"\n enter the real part of the complex :: ";
+                cin>>real;
+                cout<<"\n enter the imaginary part of the complex :: ";
+                cin>>image;
+}
+
+void complex :: operator +  (complex c1)
+{
+        complex temp;
+        temp.real=real+c1.real;
+        temp.image=image+c1.image;
+        if (temp.image>=0)
+        {
+                cout<<"\n complex no. after addition :: ";
+                cout<<temp.real<<"+"<<temp.image<<"i\n";
+        }
+        else
+        {
+                cout<<"\n complex no. after addition :: ";
+                cout<<temp.real<<temp.image<<"i\n";
+        }
+}
+
+void complex ::operator-(complex c1)
+{
+        complex temp;
+        temp.real = real-c1.image;
+        temp.image= image-c1.image;
+        if (temp.image>=0)
+        {
+                cout<<"\n complex no. after subtraction :: ";
+                cout<<"\n temp.real"<<"+"<<temp.image<<"i\n";
+        }
+        else
+        {
+                cout<<"\n complex no. after subtraction :: ";
+                cout<<temp.real<<temp.image<<"i\n";
+    }
+}
 
