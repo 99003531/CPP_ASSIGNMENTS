1b01a6759787bcfe048df3bb66c1be55863c8910
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 12:59:52 +0530

    Update Image.h

--- Image/Image.h
+++ Image/Image.h
@@ -1,1 +1,22 @@
+#ifndef _IMAGE_H_
+#define _IMAGE_H_
 
+#include <string>
+
+class Image{
+   
+        public:
+            Image();
+           Image(int,int,int,int);
+           
+           void move(int,int);
+           void scale(int);
+           void resize(int,int);
+           void dispay();
+
+        private:
+                 int m_x;
+	int m_y;
+	int m_width;
+                  int m_height; 
+};
