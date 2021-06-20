fb05f34b2208daccf488718f9fc3012484978e4e
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 13:22:39 +0530

    Update time.h

--- time_class/time.h
+++ time_class/time.h
@@ -1,1 +1,16 @@
+#ifndef _TIME_H_
+#define _TIME_H_
 
+#include <string>
+
+class time{
+   
+private: 
+	int hour, minute;
+public:
+	time();
+	time (int , int);
+	time operator++();
+   time operator++(int);
+	void display();
+};
