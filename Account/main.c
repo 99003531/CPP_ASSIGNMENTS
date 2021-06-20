0ccefe5cfebf80d2fc9388f431fa903c25074ff7
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 12:56:58 +0530

    Update main.c

--- Account/main.c
+++ Account/main.c
@@ -1,1 +1,14 @@
+#include <iostream>
 
+using namespace std;
+#include "AccountBase.h"
+int main()
+{
+
+    AccountBase *acc;
+    acc = new SavingsAccount("08","rahul",20000);
+    acc->credit();
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
