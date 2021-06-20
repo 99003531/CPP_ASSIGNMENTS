197bba6dbb18031de566cb7e9ba4aceaf289ef4a
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 13:19:25 +0530

    Update fraction.h

--- Fraction/fraction.h
+++ Fraction/fraction.h
@@ -1,1 +1,19 @@
+#ifndef _FRACTION_H_
+#define _FRACTION_H_
 
+#include <string>
+
+class fractional{
+   
+        public:
+         void getdata();
+        void operator+(fractional);
+         void operator-(fractional);
+         void operator *(fractional);
+         void operator /(fractional);
+
+        private:
+                  int numer;
+                  int denom;
+    
+};
