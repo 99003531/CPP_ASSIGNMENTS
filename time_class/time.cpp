bc7b43bc2997f264d78e9a297c2025784f2ff62c
Harshitha0961 <78536469+Harshitha0961@users.noreply.github.com>
2021-06-20 13:23:10 +0530

    Update time.cpp

--- time_class/time.cpp
+++ time_class/time.cpp
@@ -1,1 +1,52 @@
 
+#include <string>
+#include <iostream>
+#include "Time.h"
+
+
+    time::time()
+{
+	hour = 0;
+	minute = 0;
+}
+time::time(int h, int m)
+{
+	hour = h;
+	minute = m;
+}
+ time time :: operator++()
+{
+	 time t;
+	 t.minute = ++minute;
+	 t.hour = hour;
+	 if (t.minute > 59)
+		 {
+			 t.hour = ++hour;
+			 t.minute = 0;
+		 }
+		 return t;
+	 
+}
+time time :: operator++(int)
+{
+	time t1;
+	t1.minute = minute++;
+	t1.hour = hour;
+	if (t1.minute > 59)
+	{
+		t1.hour = ++hour;
+		t1.minute = 0;
+	}
+	return t1;
+}
+void time::display()
+{
+	if (minute >= 0 && minute <= 60 && hour>=1 && hour <=12 )
+	{
+		cout << "\n \n Hours : " << hour;
+		cout << "\n minutes : " << minute;
+	}
+	else
+		cout << "\n \n minute can't be greater than 60 ";
+}
+     
